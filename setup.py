2b1c2b0b127070216b6684487e4facf9304266b1
pj.ren <0403114076@163.com>
2023-03-15 16:47:02 +0800

    Version to v0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='thunder_ase',
-    version='0.2',
+    version='0.2.1',
     packages=['thunder_ase'],
     url='https://gitee.com/pjren/thunder-ase',
     license='',
