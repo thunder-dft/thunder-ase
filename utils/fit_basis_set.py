bdd56ddeb5f0e6c1b42361b3d07d6430594d296f
pj.ren <0403114076@163.com>
2023-03-22 19:18:21 +0800

    Update fit_basis_set.py

--- utils/fit_basis_set.py
+++ utils/fit_basis_set.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
+import random
 
 
 # read wf file
@@ -30,6 +31,63 @@ def loss_function(x, *args):
     return loss
 
 
+def fit_wf(data, n=1, tol=None, Nzeta=None, bnds=None):
+    """
+
+    :param data: shape = [N, 2]
+    :param n: principal quantum number
+    :param tol: Error tolerance
+    :param Nzeta: number of gaussian
+    :param bnds: boundary for fitting
+    :return:
+    """
+    R, Y = np.asarray(data)
+    if Nzeta is None:
+        if tol is None:
+            tol = 1.0E-5
+        Nzeta0 = 3  # initial zeta number
+        Nzeta = 20  # so the maximum Nzeta is 20
+    else:
+        Nzeta0 = Nzeta
+
+    success = False
+    for nz in range(Nzeta0, Nzeta+1):
+        A0 = np.random.rand(nz)  # initial value for A
+        a0 = np.random.rand(nz)  # initial value for alpha0
+        x0 = np.concatenate([A0, a0])  # initial para vector
+        if bnds is not None:
+            bnds = [bnds[0]] * nz + [bnds[1]] * nz
+        res = minimize(loss_function, x0, bounds=bnds, args=(n, R, Y))
+        Ae = res.x[0:nz]
+        ae = res.x[nz:]
+        Y_fit = gaussian(R, n, Ae, ae)
+        error = (np.linalg.norm(Y-Y_fit))/len(Y)
+        if tol is not None:
+            if error < tol:
+                success = True
+                break
+
+    if not success:
+        if tol is None:
+            print("Fitting error is {} for {} gaussians.".format(error, Nzeta))
+        else:
+            print("Warning: Fitting error {} didn't meet the tolerance {} for {} gaussians.".format(error, tol, Nzeta))
+
+    return [Ae, ae, Y_fit]
+
+
+def plot_fitting(data, Ae, ae, output=None):
+    R, Y = np.asarray(data)
+    plt.plot(R, Y, '-')
+    plt.plot(R, gaussian(R, n, Ae, ae))
+    for Ai, ai in zip(Ae, ae):
+        plt.plot(R, gaussian(R, n, [Ai], [ai]))
+    if output is not None:
+        plt.savefig(output)
+    else:
+        plt.show()
+
+
 if "__name__" == "__main__":
     import argparse
 
@@ -42,26 +100,10 @@ if "__name__" == "__main__":
 filename = '../tests/test_utils_fit_basis_set/007.wf-p1.dat'
 
 n = 1
-A0 = [-1.0, 1.0, 2.0, 3.0]
-a0 = [1.1, 1.0, 2.0, 3.0]
-# bnds = [(0, None)] * len(A0) + [(0, None)] * len(a0)
+data = read_wf(filename)
 bnds = None
 
-data = read_wf(filename)
-R, Y = data.T
-x0 = np.asarray(A0 + a0)
+Ae, ae, Y_fit = fit_wf(data=data.T, n=n, bnds=bnds, tol=1.0E-2)
+plot_fitting(data.T, Ae, ae, output=None)
 
-res = minimize(loss_function, x0, bounds=bnds, args=(n, R, Y))
-
-print(res.message)
-
-# plot result
-len_x = int(len(x0) / 2)
-Ae = res.x[0:len_x]
-ae = res.x[len_x:]
-plt.plot(R, Y, '-')
-plt.plot(R, gaussian(R, n, Ae, ae))
-for Ai, ai in zip(Ae, ae):
-    plt.plot(R, gaussian(R, n, [Ai], [ai]))
-plt.show()
 print("Done")
