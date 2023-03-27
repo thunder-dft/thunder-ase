93e5c473a2f7f031436749fe1d90ad649b3aaba5
PJ <renpj@users.noreply.github.com>
2023-03-27 14:49:18 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='thunder_ase',
     version='0.2.2',
     packages=['thunder_ase'],
-    url='https://gitee.com/pjren/thunder-ase',
-    license='',
-    author='ren',
+    url='https://github.com/thunder-dft/thunder-ase',
+    license='LGPL',
+    author='renpj',
     author_email='openrpj@gmail.com',
     description='ASE calculator interface for FIREBALL code.'
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
