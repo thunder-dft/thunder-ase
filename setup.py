5bca0f62bcd2bd195fa58b9bd102f761a1e7d6ad
PJ <renpj@users.noreply.github.com>
2023-03-27 14:17:27 +0800

    update version 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='thunder_ase',
-    version='0.2.1',
+    version='0.2.2',
     packages=['thunder_ase'],
     url='https://gitee.com/pjren/thunder-ase',
     license='',
