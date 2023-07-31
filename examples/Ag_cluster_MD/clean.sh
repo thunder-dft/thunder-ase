f1db0434bb656fea40f031772b4fc55b3b32b341
pj.ren <0403114076@163.com>
2023-07-31 17:41:58 +0800

    Add MD simulation for Ag cluster

---
+++ examples/Ag_cluster_MD/clean.sh
@@ -0,0 +1,15 @@
+rm *.CHARGES \
+   0*.inp \
+   0*.log \
+   0*.xyz \
+   Fdata.inp \
+   structures.inp \
+   structures.output \
+   0*.eigen \
+   0*.json \
+   output.log \
+   structures.options \
+   structures.xsfoptions \
+   md.log \
+   md.traj \
+   WARNINGS
