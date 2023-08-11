bc998203e79f95868788402982fd069e3bf32b7c
pj.ren <0403114076@163.com>
2023-08-11 17:19:13 +0800

    Socket run example

---
+++ examples/socket_run/clean.sh
@@ -0,0 +1,14 @@
+rm *.CHARGES \
+   0*.inp \
+   0*.log \
+   0*.xyz \
+   Fdata.inp \
+   structures.inp \
+   structures.output \
+   0*.eigen \
+   0*.json \
+   output.log \
+   structures.options \
+   WARNINGS \
+   opt*log \
+   opt*traj
