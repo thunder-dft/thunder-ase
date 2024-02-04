fe386f2d6fce418a78bd7275ddcc78e07cf62197
PJ <0403114076@163.com>
2024-02-04 16:05:11 +0800

    Clean the code, move some functions to utils

--- thunder_ase/fireball.py
+++ thunder_ase/fireball.py
@@ -7,7 +7,7 @@ from ase.calculators.socketio import SocketIOCalculator
 from ase.units import Hartree
 import numpy as np
 from ase.calculators.calculator import Calculator, CalculationFailed, all_changes, PropertyNotImplementedError
-from ase.dft.kpoints import monkhorst_pack, kpoint_convert
+from ase.dft.kpoints import kpoint_convert
 import ase.spacegroup
 from ase.io import jsonio
 from ase.dft.kpoints import BandPath
@@ -15,64 +15,7 @@ from thunder_ase.utils.mwfn import MWFN_FORMAT, MWFN_DEFAULT, MWFN_TEMPLATE, \
     CELL_TEMPLATE, format_data, read_cdcoeffs, reorder_cdcoeffs
 from thunder_ase.utils.basis_set import read_info, read_gaussian
 from thunder_ase.utils.shell_dict import SHELL_PRIMARY_NUMS, SHELL_PRIMITIVE
-import spglib
-
-
-def atoms2spg(atoms):
-    """
-    Convert ASE Atoms object to spglib input cell format
-    :param atoms: ASE Atoms object
-    :return: spglib input cell format
-    """
-    lattice = np.array(atoms.get_cell().T, dtype='double', order='C')
-    positions = np.array(atoms.get_scaled_positions(), dtype='double', order='C')
-    numbers = np.array(atoms.get_atomic_numbers(), dtype='intc')
-    cell = (lattice, positions, numbers)
-    return cell
-
-
-def get_kpts(atoms, size=None, offset=None, reduced=True, **kwargs):
-    """
-    Get irreducible kpoints.
-    Reference:
-    * https://spglib.readthedocs.io/en/latest/python-spglib.html#methods-kpoints
-    :param reduced:
-    :param offset:
-    :param atoms:
-    :param size:
-    :param gamma:
-    :return: np.array with shape (N, 4), coordinates and weights
-    """
-    if 'symprec' in kwargs:
-        symprec = kwargs['symprec']
-    else:
-        symprec = 1e-05  # for Cartesian coordinate in real space
-    if 'gamma' in kwargs:
-        gamma = kwargs['gamma']
-    else:
-        gamma = True
-    if gamma:
-        is_shift = [0, 0, 0]
-        if offset is None:
-            offset = [0., 0., 0.]
-    else:
-        is_shift = [1, 1, 1]
-        if offset is None:
-            offset = [0.5, 0.5, 0.5]
-
-    mapping, grid = spglib.get_ir_reciprocal_mesh(size, atoms2spg(atoms), is_shift=is_shift, symprec=symprec)
-    N = len(grid)
-    if reduced:
-        ir_idx = np.unique(mapping)
-        ir_grid = (grid[ir_idx] + offset) / np.asarray(size)
-    else:
-        ir_idx = range(N)
-        ir_grid = (grid + offset) / np.asarray(size)
-    kpoints = kpoint_convert(cell_cv=atoms.cell, skpts_kc=ir_grid)  # convert from scaled and cartesian coordinates
-    weights = [sum(mapping == i) / N for i in ir_idx]
-
-    kpoints_weight = [[k[0], k[1], k[2], w] for k, w in zip(kpoints, weights)]
-    return kpoints_weight
+from thunder_ase.utils import get_kpts
 
 
 options_params = {
--- thunder_ase/utils/__init__.py
+++ thunder_ase/utils/__init__.py
@@ -1,3 +1,9 @@
+import numpy as np
+import spglib
+from ase.dft.kpoints import kpoint_convert
+from thunder_ase.utils.basis_set import fit_gaussian
+
+
 def ordinal(n: int):
     """
     Convert an integer to "1st", "2nd", etc,
@@ -11,4 +17,57 @@ def ordinal(n: int):
     return str(n) + suffix
 
 
-from thunder_ase.utils.basis_set import fit_gaussian
+def atoms2spg(atoms):
+    """
+    Convert ASE Atoms object to spglib input cell format
+    :param atoms: ASE Atoms object
+    :return: spglib input cell format
+    """
+    lattice = np.array(atoms.get_cell().T, dtype='double', order='C')
+    positions = np.array(atoms.get_scaled_positions(), dtype='double', order='C')
+    numbers = np.array(atoms.get_atomic_numbers(), dtype='intc')
+    cell = (lattice, positions, numbers)
+    return cell
+
+
+def get_kpts(atoms, size=None, offset=None, reduced=True, **kwargs):
+    """
+    Get irreducible kpoints.
+    Reference:
+    * https://spglib.readthedocs.io/en/latest/python-spglib.html#methods-kpoints
+    :param reduced:
+    :param offset:
+    :param atoms:
+    :param size:
+    :return: np.array with shape (N, 4), coordinates and weights
+    """
+    if 'symprec' in kwargs:
+        symprec = kwargs['symprec']
+    else:
+        symprec = 1e-05  # for Cartesian coordinate in real space
+    if 'gamma' in kwargs:
+        gamma = kwargs['gamma']
+    else:
+        gamma = True
+    if gamma:
+        is_shift = [0, 0, 0]
+        if offset is None:
+            offset = [0., 0., 0.]
+    else:
+        is_shift = [1, 1, 1]
+        if offset is None:
+            offset = [0.5, 0.5, 0.5]
+
+    mapping, grid = spglib.get_ir_reciprocal_mesh(size, atoms2spg(atoms), is_shift=is_shift, symprec=symprec)
+    N = len(grid)
+    if reduced:
+        ir_idx = np.unique(mapping)
+        ir_grid = (grid[ir_idx] + offset) / np.asarray(size)
+    else:
+        ir_idx = range(N)
+        ir_grid = (grid + offset) / np.asarray(size)
+    kpoints = kpoint_convert(cell_cv=atoms.cell, skpts_kc=ir_grid)  # convert from scaled and cartesian coordinates
+    weights = [sum(mapping == i) / N for i in ir_idx]
+
+    kpoints_weight = [[k[0], k[1], k[2], w] for k, w in zip(kpoints, weights)]
+    return kpoints_weight
