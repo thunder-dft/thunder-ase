aa75c39af13e1d75d2eb2521076e66c6493648d5
PJ <0403114076@163.com>
2024-03-22 00:29:12 +0800

    add RattleBFGS

--- thunder_ase/optimize.py
+++ thunder_ase/optimize.py
@@ -2,10 +2,15 @@
 Due to the FIREBALL use Harris functional, the fmax as convergence criteria is too strict.
 Herein, we rewrite some of the optimizer by using force rms as criteria.
 """
+from os.path import isfile
 from types import MethodType
 
 import numpy as np
-from ase.optimize import BFGS
+from ase.io.jsonio import read_json, write_json
+from ase.optimize import (MDMin, FIRE, LBFGS, LBFGSLineSearch, BFGSLineSearch, BFGS,
+                          GoodOldQuasiNewton, QuasiNewton, GPMin, Berny, ODE12r)
+from ase.optimize.optimize import Optimizer, RestartError
+from ase.parallel import world
 
 
 def rms_converged(self, forces=None):
@@ -23,4 +28,75 @@ def rms_converged(self, forces=None):
     return rms2 < self.fmax ** 2
 
 
-BFGS.converged = MethodType(rms_converged, BFGS)
+# This is a new optimizer using RMS converge
+
+for opter in (
+        MDMin, FIRE, LBFGS, LBFGSLineSearch, BFGSLineSearch, BFGS, GoodOldQuasiNewton, QuasiNewton, GPMin, Berny,
+        ODE12r):
+    opter.converged = MethodType(rms_converged, opter)
+
+
+class RattleBFGS(BFGS):
+
+    """
+    Since the force from Harris functional is not correct, we can mimic the quantum movement by
+    using ase.Atoms.rattle, and then average the force.
+    """
+    def __init__(self, atoms, restart=None, logfile='-', trajectory=None,
+                 maxstep=None, master=None, alpha=None, rattle=3):
+
+        self.rattle = rattle
+
+        BFGS.__init__(self, atoms, restart=restart, logfile=logfile, trajectory=trajectory,
+                      maxstep=maxstep, master=master, alpha=alpha)
+
+    def initialize(self):
+        BFGS.initialize(self)
+        if not isfile('force.rattle'):
+            self.dump_f0(0.0)
+
+    def todict(self):
+        d = BFGS.todict(self)
+        d.update(rattle=self.rattle)
+        return d
+
+    def get_forces(self):
+        # read old force from file
+        f0 = self.read_f0()
+        # read new force from atoms
+        f1 = self.atoms.get_forces()
+        f = f0 + f1
+        # write new force to file
+        self.dump_f0(f)
+        return f
+
+    def dump_f0(self, data):
+        if world.rank == 0 and self.rattle != 1:
+            with open('force.rattle', 'w') as fd:
+                write_json(fd, data)
+
+    def read_f0(self):
+        with open('force.rattle') as fd:
+            try:
+                f0 = read_json(fd, always_array=True)
+            except Exception as ex:
+                msg = ('Could not decode temp force file as JSON.  '
+                       f'You may need to delete the restart file force.rattle')
+                raise RestartError(msg) from ex
+        return f0
+
+    def step(self, f=None):
+        atoms = self.atoms
+
+        if f is None:
+            f = self.get_forces()
+
+        if self.nsteps % self.rattle == 0:
+            # run BFGS step
+            BFGS.step(self, f)
+            # write 0.0 to new force.rattle
+            self.dump_f0(0.0)
+        else:
+            # rattle atoms position with a very small displacement
+            atoms.rattle(0.00001)
+
