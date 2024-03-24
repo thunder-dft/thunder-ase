a4c47238933f511be3f2cef4abe276f14bfed234
PJ <0403114076@163.com>
2024-03-24 11:49:57 +0800

    Re-write BFGS determine_step function

--- thunder_ase/optimize.py
+++ thunder_ase/optimize.py
@@ -12,6 +12,8 @@ from ase.optimize import (MDMin, FIRE, LBFGS, LBFGSLineSearch, BFGSLineSearch, B
 from ase.optimize.optimize import Optimizer, RestartError
 from ase.parallel import world
 
+from ase.md.velocitydistribution import ZeroRotation, Stationary
+
 
 def rms_converged(self, forces=None):
     """Did the optimization converge?"""
@@ -37,11 +39,11 @@ for opter in (
 
 
 class RattleBFGS(BFGS):
-
     """
     Since the force from Harris functional is not correct, we can mimic the quantum movement by
     using ase.Atoms.rattle, and then average the force.
     """
+
     def __init__(self, atoms, restart=None, logfile='-', trajectory=None,
                  maxstep=None, master=None, alpha=None, rattle=3):
 
@@ -78,7 +80,7 @@ class RattleBFGS(BFGS):
     def read_f0(self):
         with open('force.rattle') as fd:
             try:
-                f0 = read_json(fd, always_array=True)
+                f0 = read_json(fd, always_array=False)
             except Exception as ex:
                 msg = ('Could not decode temp force file as JSON.  '
                        f'You may need to delete the restart file force.rattle')
@@ -88,15 +90,49 @@ class RattleBFGS(BFGS):
     def step(self, f=None):
         atoms = self.atoms
 
-        if f is None:
-            f = self.get_forces()
+        # if self.nsteps % self.rattle == 0:
+        #    print("Step {}, BFGS step.".format(self.nsteps))
+        #    # run BFGS step
+        #    BFGS.step(self, f)
+        #    # write 0.0 to new force.rattle
+        #    self.dump_f0(0.0)
+        # else:
+        #    # rattle atoms position with a very small displacement
+        #    print("Step {}, Rattle step.".format(self.nsteps))
+        #    atoms.rattle(0.0001)
 
         if self.nsteps % self.rattle == 0:
-            # run BFGS step
-            BFGS.step(self, f)
+            print("Step {}, BFGS step.".format(self.nsteps))
             # write 0.0 to new force.rattle
+            f = self.get_forces()
             self.dump_f0(0.0)
         else:
             # rattle atoms position with a very small displacement
-            atoms.rattle(0.00001)
+            print("Step {}, Rattle step.".format(self.nsteps))
+            f = atoms.get_forces()
+            _ = self.get_forces()  # update force
+        # run BFGS step
+        BFGS.step(self, f)
 
+
+def determine_step(self, dr, steplengths):
+    """Re-write BFGS this function
+    """
+    maxsteplength = np.max(steplengths)
+    if maxsteplength >= self.maxstep:
+        scale = self.maxstep / maxsteplength
+        # FUTURE: Log this properly
+        # msg = '\n** scale step by {:.3f} to be shorter than {}'.format(
+        #     scale, self.maxstep
+        # )
+        # print(msg, flush=True)
+
+        dr *= scale
+
+    atoms_tmp = self.atoms.copy()
+    atoms_tmp.set_velocities(dr)
+    Stationary(atoms_tmp, False)
+    ZeroRotation(atoms_tmp, False)
+    dr = atoms_tmp.get_velocities()
+
+    return dr
